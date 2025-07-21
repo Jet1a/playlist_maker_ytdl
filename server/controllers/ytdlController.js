1677c191b29692e211cab3221a13c6ae0316b5a6
Jettaphum Chantaweekhun <jettaphoom@gmail.com>
2025-07-21 19:48:20 +0700

    feat: Multiple videos download

--- server/controllers/ytdlController.js
+++ server/controllers/ytdlController.js
@@ -1,4 +1,9 @@
 import ytdl from 'ytdl-core'
+import fs from 'fs'
+import path from 'path'
+import archiver from 'archiver'
+
+const DOWNLOAD_DIR = path.resolve('./downloads')
 
 export const downloadVideo = async (req, res) => {
    const videoUrl = req.query.url
@@ -35,4 +40,62 @@ export const getVideoInfo = async (req, res) => {
          name: info.videoDetails.author.name
       }
    })
+}
+
+export const downloadMultipleVideos = async (req, res) => {
+   const { urls } = req.body
+
+   if (!Array.isArray(urls) || urls.length === 0) {
+      return res.status(400).json({ error: "No URLs provided." })
+   }
+
+   // Clean & Recreate download folder
+   if (fs.existsSync(DOWNLOAD_DIR)) {
+      fs.rmSync(DOWNLOAD_DIR, { recursive: true })
+   }
+   fs.mkdirSync(DOWNLOAD_DIR)
+
+   try {
+      const downloadPromise = urls.map(async (url, index) => {
+         if (!ytdl.validateURL(url)) {
+            return
+         }
+
+         const info = await ytdl.getInfo(videoUrl)
+         const title = info.videoDetails.title.replace(/[^a-z0-9ก-๏]+/gi, '_').toLowerCase()
+
+         const filename = `${index + 1}_${title}.mp4`
+         const filepath = path.join(DOWNLOAD_DIR, filename)
+
+         return new Promise((resolve, reject) => {
+            ytdl(url, { quality: 'lowest' })
+               .pipe(fs.createWriteStream(filepath))
+               .on('finish', resolve)
+               .on('error', reject)
+         })
+      })
+
+      await Promise.all(downloadPromise)
+
+      // ZIP
+      const zipName = 'videos.zip'
+      const zipPath = path.join(DOWNLOAD_DIR, zipName)
+      const output = fs.createWriteStream(zipPath)
+      const archive = archiver('zip', { zlib: { level: 9 } })
+
+      output.on('close', () => {
+         res.download(zipPath, zipName, () => {
+            fs.rmSync(DOWNLOAD_DIR, { recursive: true })
+         })
+      })
+
+      archive.on('error', (err) => res.status(500).json({ error: err.message }))
+
+      archive.pipe(output)
+      archive.directory(DOWNLOAD_DIR, false)
+      archive.finalize()
+   } catch (error) {
+      console.error(error)
+      return res.status(500).json({ error: "Failed to process videos." })
+   }
 }
--- server/routes/ytdlRoutes.js
+++ server/routes/ytdlRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { downloadVideo, getVideoInfo } from '../controllers/ytdlController.js'
+import { downloadMultipleVideos, downloadVideo, getVideoInfo } from '../controllers/ytdlController.js'
 
 const router = express.Router()
 
 router.get("/download", downloadVideo)
 router.get("/info", getVideoInfo)
+router.post("/download-multiple", downloadMultipleVideos)
 
 export default router
